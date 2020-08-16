aa6fb0b0994f579072c936d3f93891ebbc68aea4
hrhr49 <hiro49410@gmail.com>
2020-08-16 20:58:19 +0900

    add entory point file.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {createFlowchart, Flowchart} from './flowchart'
+import {defaultConfig, Config, mergeDefaultConfig} from './config'
+import {render, Renderer} from './renderer/svg'
+
+export {
+  render,
+  Renderer,
+  createFlowchart,
+  Flowchart,
+  defaultConfig,
+  Config,
+  mergeDefaultConfig,
+}
