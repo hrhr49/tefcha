08f81535d69d68ad44f5dd5bce8cacea626eec73
hrhr49 <hiro49410@gmail.com>
2021-07-08 21:22:12 +0900

    add test script for fuzzing.

---
+++ tests/fuzzing/create_random_src.ts
@@ -0,0 +1,209 @@
+import * as fs from 'fs'
+
+import {parse} from '../../src/parser'
+import {defaultConfig} from '../../src/config'
+
+const randRange = (start: number, end: number): number => {
+  return start + Math.floor(Math.random() * (end - start));
+};
+
+const randChoice = (array: any[]): any => {
+  return array[randRange(0, array.length)];
+};
+
+const randDist = (total: number, num: number, min: number = 1): number[] => {
+  console.assert(min * num <= total);
+  const distArray = new Array(num).fill(min);
+  let restNum = total - min * num;
+  for (let i = 0; i < restNum; i++) {
+    distArray[randRange(0, num)]++;
+  }
+  return distArray;
+};
+
+const randChar = (): string => {
+  return randChoice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''));
+}
+
+const randStr = (length: number = 5): string => {
+  while (true) {
+    const ret = new Array(randRange(1, length+1))
+      .fill(null)
+      .map(() => randChar())
+      .join('');
+    if (!['if', 'elif', 'else', 'while', 'switch', 'do', 'case', 'try', 'except', 'break', 'continue', 'for', 'pass'].includes(ret)) {
+      return ret;
+    }
+  }
+};
+
+const addIndent = (lines: string[]): string[] => {
+  return lines.map(s => '  ' + s);
+};
+
+const createRandomSrc = ({
+  lineNum = 10,
+  isLooping = false,
+  exceptNumMax = 3,
+  elifNumMax = 2,
+}: {
+  lineNum?: number,
+  isLooping?: boolean,
+  exceptNumMax?: number,
+  elifNumMax?: number,
+}): string[] => {
+  if (lineNum <= 0) {
+    return [];
+  }
+  const items: string[] = ['simple'];
+  if (lineNum >= 2) {
+    items.push('if', 'while');
+    if (isLooping) {
+      items.push('continue', 'break');
+    }
+  }
+  if (lineNum >= 3) {
+    items.push('do-while');
+  }
+  if (lineNum >= 4) {
+    items.push('try-except');
+  }
+
+  const item = items[randRange(0, items.length)];
+  // console.log(lineNum);
+  // console.log(item);
+  switch (item) {
+    case 'simple': {
+      return [
+        randStr(),
+        ...createRandomSrc({
+          lineNum: lineNum - 1,
+          isLooping
+        }),
+      ]
+    }
+    case 'continue': {
+      return [
+        ...createRandomSrc({
+          lineNum: lineNum - 1,
+          isLooping: false
+        }),
+        'continue',
+      ]
+    }
+    case 'break': {
+      return [
+        ...createRandomSrc({
+          lineNum: lineNum - 1,
+          isLooping: false
+        }),
+        'break',
+      ]
+    }
+    case 'if': {
+      let useElse: boolean
+      if (lineNum >= 4) {
+        useElse = randChoice([true, false]);
+      } else {
+        useElse = false;
+      }
+      const elifNum = Math.max(0, Math.min(elifNumMax, Math.floor((lineNum - 2 - (useElse ? 2 : 0)) / 2)));
+      const blockNum = 1 + (useElse ? 1 : 0) + elifNum;
+      const blockLineNumArray = randDist(lineNum - blockNum, blockNum);
+      const ret = [
+        `if ${randStr()}`,
+        ...addIndent(createRandomSrc({
+          lineNum: blockLineNumArray[0],
+          isLooping,
+        })),
+      ];
+      if (elifNum > 0) {
+        blockLineNumArray.slice(1, -1).forEach(blockLineNum => {
+          ret.push(
+            `elif ${randStr()}`,
+            ...addIndent(createRandomSrc({
+              lineNum: blockLineNum,
+              isLooping,
+            })),
+          )
+        });
+      }
+      if (useElse) {
+        ret.push(
+          'else',
+          ...addIndent(createRandomSrc({
+            lineNum: blockLineNumArray.slice(-1)[0],
+            isLooping,
+          })),
+        );
+      }
+
+      return ret;
+    }
+    case 'while': {
+      return [
+        `while ${randStr()}`,
+        ...addIndent(createRandomSrc({
+          lineNum: lineNum - 1,
+          isLooping: true,
+        })),
+      ]
+    }
+    case 'do-while': {
+      return [
+        'do',
+        ...addIndent(createRandomSrc({
+          lineNum: lineNum - 2,
+          isLooping: true,
+        })),
+        `while ${randStr()}`,
+      ]
+    }
+    case 'try-except': {
+      const exceptNum = Math.min(exceptNumMax, Math.floor((lineNum - 2) / 2));
+      const blockLineNumArray = randDist(lineNum - (1 + exceptNum), 1 + exceptNum);
+      const ret = [
+        'try',
+        ...addIndent(createRandomSrc({
+          lineNum: blockLineNumArray[0],
+          isLooping,
+        })),
+      ];
+      blockLineNumArray.slice(1).forEach(blockLineNum => {
+        ret.push(
+          `except ${randStr()}`,
+          ...addIndent(createRandomSrc({
+            lineNum: blockLineNum,
+            isLooping,
+          })),
+        )
+      });
+      return ret;
+    }
+    default: {
+      console.error(`invalid item ${item}`);
+    }
+  }
+}
+
+// for (let i = 0; i < 10000; i++) {
+//   const iStr = `0000000${i}`.slice(-4);
+//   while (true) {
+//     const lineNum = Math.floor(Math.random() * 50) + 1;
+//     const src = createRandomSrc({
+//       lineNum,
+//     }).join('\n') + '\nend\n';
+//     // try {
+//       parse(src, defaultConfig);
+//     console.log(i);
+//       // console.log(src + '\n');
+//       fs.writeFileSync(`random_src/${iStr}.txt`, src);
+//       break;
+//     // } catch {
+//     // }
+//   }
+// }
+
+export {
+  createRandomSrc,
+}
---
+++ tests/fuzzing/main.ts
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+import * as fs from 'fs'
+import * as path from 'path'
+import * as puppeteer from 'puppeteer'
+import * as rimraf from 'rimraf'
+
+import {
+  createRandomSrc,
+} from './create_random_src';
+
+import {
+  parse,
+} from '../../src/parser';
+import {
+  defaultConfig,
+} from '../../src/config';
+import {
+  checkSVGFile,
+} from './svg_extractor';
+
+declare global {
+  interface Window {
+    tefcha: any;
+  }
+}
+
+const launch = async (srcs: string[], outputFileBases: string[], config: any) => {
+  const browser = await puppeteer.launch({
+    // headless: false,
+  });
+  const page = await browser.newPage();
+  const htmlFile = path.join(__dirname, 'index.html');
+  page.setViewport({width: 500, height: 500});
+  await page.goto(`file://${htmlFile}`);
+  await page.evaluate(`document.body.style.background = '${'white'}'`);
+
+  for (let i = 0; i < srcs.length; i++) {
+    const src = srcs[i];
+    console.log(`rendering flowchart... ${i}`);
+    const result = await page.$eval('#container', (container: any, src: any, config: any, _x3: any) => {
+      container.textContent = src;
+      try {
+        window.tefcha.initialize(config);
+      } catch (error) {
+        return {status: 'error', error, message: error.message};
+      }
+    }, src, config, null);
+
+    if (result && result.status === 'error') {
+      console.error(result.message);
+    }
+
+    try {
+      const svgElement = (await page.$$('svg'))[0];
+      const outputPngFile = `result/png/${outputFileBases[i]}.png`;
+      const outputSvgFile = `result/svg/${outputFileBases[i]}.svg`;
+      await svgElement.screenshot({
+        path: outputPngFile,
+        type: 'png',
+      });
+      const svgText: string = await page.$eval('#container', container => container.innerHTML);
+      fs.writeFileSync(outputSvgFile, svgText);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  await browser.close();
+};
+
+const main = async () => {
+  const N = 1000;
+  const lineNumMax = 500;
+
+  rimraf.sync('result');
+  if (!fs.existsSync('result/random_src')) {
+    fs.mkdirSync('result/random_src', {recursive: true});
+  }
+  if (!fs.existsSync('result/png')) {
+    fs.mkdirSync('result/png', {recursive: true});
+  }
+  if (!fs.existsSync('result/svg')) {
+    fs.mkdirSync('result/svg', {recursive: true});
+  }
+  if (!fs.existsSync('result/failure/random_src')) {
+    fs.mkdirSync('result/failure/random_src', {recursive: true});
+  }
+  if (!fs.existsSync('result/failure/png')) {
+    fs.mkdirSync('result/failure/png', {recursive: true});
+  }
+  if (!fs.existsSync('result/failure/svg')) {
+    fs.mkdirSync('result/failure/svg', {recursive: true});
+  }
+
+  const srcs: string[] = [];
+  const outputFileBases: string[] = [];
+  for (let i = 0; i < N; i++) {
+    const iStr = `0000000${i}`.slice(-5);
+    outputFileBases.push(iStr);
+    const lineNum = Math.floor(Math.random() * (lineNumMax - 1)) + 1;
+    const src = createRandomSrc({
+      lineNum,
+    }).join('\n') + '\nend\n';
+    // confirm no error when parsing.
+    parse(src, defaultConfig);
+    fs.writeFileSync(`result/random_src/${iStr}.txt`, src);
+    srcs.push(src);
+  }
+  
+  await launch(srcs, outputFileBases, defaultConfig);
+
+  let ok = 0;
+  let ng = 0;
+  outputFileBases.forEach((outputFileBase, idx) => {
+    console.log(`checking flowchart... ${idx}`);
+    const svgFile = `result/svg/${outputFileBase}.svg`;
+    const pngFile = `result/png/${outputFileBase}.png`;
+    const srcFile = `result/random_src/${outputFileBase}.txt`;
+    const result = checkSVGFile(svgFile, {
+      hlineDistMax: 10 // defaultConfig.flowchart.hlineMargin - 1e-3
+    });
+    if (result !== 'OK') {
+      console.log(`fail in ${svgFile}`);
+      console.log(result);
+      fs.copyFileSync(svgFile, `result/failure/svg/${outputFileBase}.svg`);
+      fs.copyFileSync(pngFile, `result/failure/png/${outputFileBase}.png`);
+      fs.copyFileSync(srcFile, `result/failure/random_src/${outputFileBase}.txt`);
+      ng++;
+    } else {
+      ok++;
+    }
+  });
+  console.log(`OK: ${ok}/${ok+ng}, NG: ${ng}/${ok+ng}`);
+}
+
+main();
